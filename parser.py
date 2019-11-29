4e8c93b96d6b8535c46c111e28a808a647ce5f0f
ykonda <yuria.konda@tuhh.de>
2019-11-29 12:40:47 -0800

    weekday plot

--- parser.py
+++ parser.py
@@ -1,24 +1,31 @@
 from bs4 import BeautifulSoup
 from dateutil.parser import parse as date_parser
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import numpy as np
+import pandas as pd
+from os.path import abspath, join, dirname
+from os import listdir
+import seaborn as sns
+sns.set(style='white', rc={'axes.facecolor':(0, 0, 0, 0)})
+import matplotlib.pyplot as plt
+import datetime
 
 def get_date(dot_node):
     return date_parser(dot_node.parent.find('div', {'class':'date'}).attrs['title'])
 
-html = open('data/messages.html', 'r').read()
+html = open(join(dirname(abspath(__file__)), 'data/messages.html'), 'r').read()
+# html = open('data/messages.html', 'r').read()
 soup = BeautifulSoup(html)
 date_list = []
 for val in soup.find_all('div', {'class':'text'}):
     if val.text.strip() == '.':
         date_list.append(get_date(val))
 
-hours_counts = np.zeros(24)
-for date in date_list:
-    hours_counts[date.hour] += 1
+df = pd.DataFrame()
+df['date'] = date_list
+df['weekday'] = df.date.apply(lambda x: x.weekday_name)
+df['hour'] = df.date.apply(lambda x: (x.hour+9)%24)
 
-hours_counts = np.roll(hours_counts, 9)
-x_axis = np.linspace(0, 23, 24)
-plt.bar(x_axis, hours_counts)
+pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
+g = sns.FacetGrid(df, row='weekday', palette=pal)
+g.map(sns.distplot, 'hour', kde=False, bins=24)
 plt.show()
